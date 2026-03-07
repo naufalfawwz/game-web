6c32c2d88acabf4b58bb051d4300dadb64f8647c
Muhammad Naufal Fawwaz <106318487+naufalfawwz@users.noreply.github.com>
2026-03-08 07:46:08 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getDatabase, ref, set, onValue, update }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+import { getAnalytics }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyDqsLDpzRhY1_X3CGkyZdIrl_CTHNlvk3Q",
   authDomain: "trailpioneer-15.firebaseapp.com",
@@ -17,6 +18,10 @@ const firebaseConfig = {
   measurementId: "G-LZLRJZYZWK"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+
 const analytics = getAnalytics(app);
+
+export { db, ref, set, onValue, update };
